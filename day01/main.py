bd3363b74321251b0a82d4dae2d43bd89e5b6a98
Jean Furman <jean_11spfc@hotmail.com>
2023-12-07 00:30:01 -0300

    feat: Day 1 challenge completed

---
+++ day01/main.py
@@ -0,0 +1,26 @@
+full_numbers = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+}
+
+with open('puzzle_input.txt', 'r') as input:
+    sum = 0
+    numbers = []
+    for row in input:
+        for index, char in enumerate(row):
+            if char.isdigit():
+                numbers.append(int(char))
+            for i in range(3, 6):
+                v = full_numbers.get(row[index:index+i])
+                if v is not None:
+                    numbers.append(v)
+        sum += int(f"{numbers[0]}{numbers[-1]}")
+        numbers = []
+    print(f"The sum is: {sum}")
